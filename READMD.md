6f1ae2b5888a6b4c7352839bca2834bde9431a5c
sbin0819 <sbinha123@gmail.com>
2022-01-09 14:28:45 +0900

    :white_check_mark: test(settings): navigation test setup

--- READMD.md
+++ READMD.md
@@ -1,2 +1,5 @@
 ## jest.config
 https://stackoverflow.com/questions/60021119/how-can-i-stop-my-react-native-tests-bombing-out-on-flow-types-in-node-modules
+
+## navigation 테스트
+https://www.reactnativeschool.com/setup-jest-tests-with-react-navigation
---
+++ src/tabs/MainTab.test.tsx
@@ -0,0 +1,21 @@
+/**
+ * @param
+ */
+import 'react-native';
+import React from 'react';
+import {render, waitFor} from '@testing-library/react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import RootStack from '@screens/RootStack';
+
+// Note: test renderer must be required after react-native.
+
+describe('test', () => {
+  it('renders Home', async () => {
+    const {getByText} = render(
+      <NavigationContainer>
+        <RootStack />
+      </NavigationContainer>,
+    );
+    await waitFor(() => getByText('Home'));
+  });
+});
